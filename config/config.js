c03b78dee71410772797cbc6d034ea780143b6b3
Speloquin <speloqu24@gmail.com>
2021-01-21 14:25:32 -0700

    env_variable JAWSDB edit in config

--- config/config.js
+++ config/config.js
@@ -3,40 +3,37 @@ require("dotenv").config();
 const mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: "provider_db",
-  });
-}
-
-connection.connect();
-module.exports = connection;
-
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER,
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: process.env.DB_USER,
 //     password: process.env.DB_PW,
 //     database: "provider_db",
-//     host: "127.0.0.1",
-//     dialect: "mysql",
-//   },
-//   test: {
-//     username: "root",
-//     password: null,
-//     database: "database_test",
-//     host: "127.0.0.1",
-//     dialect: "mysql",
-//   },
-//   production: {
-//     username: "root",
-//     password: null,
-//     database: "database_production",
-//     host: "127.0.0.1",
-//     dialect: "mysql",
-//   },
-// };
+//   });
+// }
+
+// connection.connect();
+// module.exports = connection;
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: "provider_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql",
+  },
+};
